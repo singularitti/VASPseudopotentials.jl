e3c1b5e110110e896785ccf0f14bdf22c77a43f1
singularitti <singularitti@outlook.com>
2025-08-01 21:49:55 -0500

    Add "Test parsing valence electron configurations"

--- test/parse.jl
+++ test/parse.jl
@@ -11,3 +11,25 @@ files = abspath.(readdir(joinpath(@__DIR__, "../data"); join=true))
         @test_skip isempty(symdiff(parsed, bench))  # Does not work for VASP <= 5.2
     end
 end
+
+@testset "Test parsing valence electron configurations" begin
+    for file in files
+        df = CSV.read(file, DataFrame)
+        parsed = map(eachrow(df)) do row
+            value = row["Valence electron configuration"]
+            if value === missing
+                missing
+            else
+                mapreduce(
+                    Base.Fix1(parse, Subshell),
+                    *,
+                    eachsplit(row["Valence electron configuration"]; keepempty=false);
+                    init=Subshell[],
+                )
+            end
+        end
+        total = map(count_valence_electrons, parsed)
+        bench = df[:, "Number of valence electrons"]
+        @test total ≈ bench atol = 1.1
+    end
+end
