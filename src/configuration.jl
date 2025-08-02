f4d854dc93abf1c8d0054b98dd676807b981f822
singularitti <singularitti@outlook.com>
2025-08-01 21:49:08 -0500

    Deprecate `ValenceElectronConfiguration`, redefine `AbstractVector{Subshell}`, add `count_valence_electrons` method

--- src/configuration.jl
+++ src/configuration.jl
@@ -1,6 +1,6 @@
 using AnonymousEnums: @anonymousenum
 
-export AzimuthalQuantumNumber, Subshell
+export AzimuthalQuantumNumber, Subshell, count_valence_electrons
 
 @anonymousenum AzimuthalQuantumNumber::UInt8 s p d f g h i
 
@@ -10,13 +10,14 @@ struct Subshell
     occupation::Float64
 end
 
-const ValenceElectronConfiguration = AbstractVector{Subshell}
+count_valence_electrons(config) = sum(s.occupation for s in config)
+count_valence_electrons(subshell::Subshell) = subshell.occupation
+count_valence_electrons(::Missing) = 0
 
 Base.:*(a::Subshell, b::Subshell) = [a, b]
-Base.:*(a::Subshell, b::ValenceElectronConfiguration) = [a; b]
-Base.:*(a::ValenceElectronConfiguration, b::Subshell) = [a; b]
-Base.:*(a::ValenceElectronConfiguration, b::ValenceElectronConfiguration) = [a; b]
+Base.:*(a::Subshell, b::AbstractVector) = [a; b]
+Base.:*(a::AbstractVector, b::Subshell) = [a; b]
+Base.:*(a::AbstractVector{Subshell}, b::AbstractVector{Subshell}) = [a; b]
 
 Base.show(io::IO, s::Subshell) = print(io, "$(s.principal)$(s.azimuthal)^{$(s.occupation)}")
-Base.show(io::IO, ::MIME"text/plain", c::ValenceElectronConfiguration) =
-    print(io, join(c, " "))
+Base.show(io::IO, ::MIME"text/plain", c::AbstractVector{Subshell}) = print(io, join(c, " "))
